f985998734f519110fda336d6167eada651de449
André Luiz Pardal <alpardal@gmail.com>
2016-07-08 10:52:57 -0300

    first

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack'),
+      path = require('path');
+
+const PATHS = {
+  main: './src/main.js',
+  html: './index.html',
+  dist: path.join(__dirname, 'dist')
+};
+
+module.exports = {
+  entry: {
+    javascript: PATHS.main,
+    html: PATHS.html
+  },
+  output: {
+    path: PATHS.dist,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: PATHS.dist
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      }
+    ]
+  }
+};
